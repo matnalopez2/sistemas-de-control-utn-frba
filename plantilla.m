27d6a3cf9ca915b8b8eaaceaee0ec457ce7fc8af
Matías N. López <matias@matnalopez.com.ar>
2026-02-09 10:07:15 -0300

    agrego plantilla ejercicios

---
+++ plantilla.m
@@ -0,0 +1,32 @@
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Ejercicio X 
+% Realizado por: Matías Nahuel López
+% 
+% Universidad Tecnológica Nacional
+% Facultad Regional de Buenos Aires
+% 
+% Legajo: 153.327-7
+% Contacto: matias@matnalopez.com.ar
+% 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Inicialización
+
+% Generales
+clear; clc; close all;
+s = tf('s');
+
+% Para gráficos
+T_inicio    = 0;
+delta_t     = 0.01;
+T_final     = 8;
+t           = (T_inicio:delta_t:T_final)';
+
+
+%% Datos del problema
+
+
+
+%% Resolución
+
